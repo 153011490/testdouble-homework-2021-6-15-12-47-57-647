5aa541da37ef1ee1c50b125f4a929d20c10f03eb
153011490 <153011490@qq.com>
2021-06-28 22:08:54 +0800

    add test method named should_console_printLine_be_called_when_printer_printStatementLines

--- src/test/java/com/tw/banking/PrinterTest.java
+++ src/test/java/com/tw/banking/PrinterTest.java
@@ -2,6 +2,8 @@ package com.tw.banking;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +22,22 @@ class PrinterTest {
         printer.print(transactions);
 
         verify(console,times(1)).printLine(STATEMENT_HEADER);
+    }
+
+    @Test
+    public void should_console_printLine_be_called_when_printer_printStatementLines() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Console console = mock(Console.class);
+        Printer printer = new Printer(console);
+        Transaction transaction = new Transaction("28/06/2021",1);
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(transaction);
+        Class<? extends Printer> clazz = printer.getClass();
+
+        Method method = clazz.getDeclaredMethod("printStatementLines", List.class);
+        method.setAccessible(true);
+        method.invoke(printer,transactions);
+
+        verify(console,times(1)).printLine(anyString());
     }
 
 }
