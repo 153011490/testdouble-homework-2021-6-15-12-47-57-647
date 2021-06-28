543437476d9b24ca8d9fa1bc7802345cda9768ab
153011490 <153011490@qq.com>
2021-06-28 21:47:20 +0800

    add test method named should_sub_amount_when_transactionRepository_addWithdraw_given_date_and_amount

--- src/test/java/com/tw/banking/TransactionRepositoryTest.java
+++ src/test/java/com/tw/banking/TransactionRepositoryTest.java
@@ -3,6 +3,7 @@ package com.tw.banking;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static java.util.Collections.unmodifiableList;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -20,6 +21,22 @@ class TransactionRepositoryTest {
         transactionRepository.addDeposit(amount);
         //then
         Assertions.assertEquals(amount,transactionRepository.transactions.get(0).amount());
+        Assertions.assertEquals(date,transactionRepository.transactions.get(0).date());
+    }
+
+
+    @Test
+    public void should_sub_amount_when_transactionRepository_addWithdraw_given_date_and_amount(){
+        //given
+        Clock clock = mock(Clock.class);
+        TransactionRepository transactionRepository = new TransactionRepository(clock);
+        String date = "28/06/2021";
+        when(clock.todayAsString()).thenReturn(date);
+        int amount = 1;
+        //when
+        transactionRepository.addWithdraw(amount);
+        //then
+        Assertions.assertEquals(-amount,transactionRepository.transactions.get(0).amount());
         Assertions.assertEquals(date,transactionRepository.transactions.get(0).date());
     }
 
